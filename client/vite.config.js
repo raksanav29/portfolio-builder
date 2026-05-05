3de9498ce0e77024b5afa4445f207dda4e56e36a
Raksana <raksana2907@gmail.com>
2026-05-05 12:12:46 +0530

    Fix chunk size warning

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600, // increase limit
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split large libraries into separate chunks
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          motion: ['framer-motion'],
+          axios: ['axios'],
+        },
+      },
+    },
+  },
 })
