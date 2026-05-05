c461373c9d7b3e281628e76f9be60d36e7ecd77e
Raksana <raksana2907@gmail.com>
2026-05-05 13:26:51 +0530

    fixed cloudinary v2 config

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,24 +1,9 @@
-// const cloudinary = require("cloudinary").v2;
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key:    process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-// module.exports = cloudinary;
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:    process.env.CLOUDINARY_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure:     true,
 });
-
-// Test the config on startup
-cloudinary.api.ping()
-  .then(() => console.log("✅ Cloudinary connected"))
-  .catch((err) => console.error("❌ Cloudinary error:", err.message));
 
 module.exports = cloudinary;
